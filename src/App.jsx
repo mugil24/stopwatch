cdef762d783a84db3a78a01f31020d1a23240bf6
mugil24 <vaanmugil24@gmail.com>
2024-07-19 15:26:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TimerDemo from './assets/components/TimerDemo/TimerDemo'
+
+function App() {
+  return (
+    <React.Fragment>
+      <TimerDemo/>
+
+    </React.Fragment>
+
+  )
+}
+
+export default App
