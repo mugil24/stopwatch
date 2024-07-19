385e1dd5a661375a112f0f8559a9a8e21051bac6
mugil24 <vaanmugil24@gmail.com>
2024-07-19 20:49:57 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/stopwatch/', // This is the base URL of your GitHub Pages site
+});
